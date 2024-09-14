afb0a22ddef416013a5e95d501e244da49966ea5
randomboy24 <jatinthegod21@gmail.com>
2024-09-14 23:56:23 +0530

    ..ui stuf

--- src/components/walletSelection.tsx
+++ src/components/walletSelection.tsx
@@ -3,15 +3,15 @@
 export const WalletSelection = ({setIsEthereum,setIsSolana}:any) => {
     return (
         <div className="flex justify-center">
-            <div>
-                <div className="text-white text-5xl font-bold mt-24 ml-2 md:ml-52 max-w-fit md:w-3/6">
+            <div className="md:w-[67%] w-[100%] border border-white mt-32" >
+                <div className="text-white text-5xl font-bold  ml-2  w-[90%] md:w-[65%]">
                 WebVault: The vault where your crypto and peace of mind coexist.
                 </div>
-                <div className="  text-2xl text-gray-300 ml-2 md:ml-52 mt-3">
+                <div className="  text-2xl text-gray-300 ml-2  mt-3">
                     Choose a blockchain to get started.
                 </div>
                 <div className="mt-5">
-                    <button className="text-black bg-white ml-2 md:ml-52 h-12 w-36 hover:bg-gray-800 hover:text-gray-200 rounded-lg transform transition-transform duration-300 hover:scale-105" onClick={() => {
+                    <button className="text-black bg-white ml-2 h-12 w-36 hover:bg-gray-800 hover:text-gray-200 rounded-lg transform transition-transform duration-300 hover:scale-105" onClick={() => {
                         setIsSolana(true)
                         localStorage.setItem('isSolana','true')
                     }}>
